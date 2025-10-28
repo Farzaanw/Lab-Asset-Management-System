9b463773ef42b30726fb47718e288e2ee30c86b1
gilbertboys <91152662+gilbertboys@users.noreply.github.com>
2025-10-28 15:10:35 -0700

    Update FacultyResearcher.cpp
    uncapitalized splish

--- src/FacultyResearcher/FacultyResearcher.cpp
+++ src/FacultyResearcher/FacultyResearcher.cpp
@@ -2,23 +2,24 @@
 // Auth: Dunlap, Jack
 // Supports: UR-101, UR-102, UR-103, UR-104
 // Collaborators: ResearchStudent[1..*], Assets[*], Reservation[*], Documents[*] 
+
 #include <string>
 #include <vector>
 
 class FacultyResearcher {
-Private:
+private:
   int FacultyID;
   int labGroupID;
-Public:
-  Std::string Fname;
-  Std::string Lname;
-  Std::string email;
-  Bool permissions;
+public:
+  std::string Fname;
+  std::string Lname;
+  std::string email;
+  bool permissions;
 	
 	//make a reservation for an asset
-	Bool reserveAsset(int assetID, bool permissions) {returns bool - was it successful in making reservation} 
+	bool reserveAsset(int assetID, bool permissions) //returns bool - was it successful in making reservation}
 	//produce a usage report for resource usage by a group
-	Document generateUsageReport(assetUsed, dataRecords) {returns document structure}
+	Document generateUsageReport(assetUsed, dataRecords) //returns document structure
 	//display a record of the group the faculty is in charge of and their work they have done
-	Void viewGroup(LabGroupID);
+	void viewGroup(LabGroupID);
 }
