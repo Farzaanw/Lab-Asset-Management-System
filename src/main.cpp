f07cc55bc0ba2c12a0800000306e391b757faa4c
gilbertboys <91152662+gilbertboys@users.noreply.github.com>
2025-10-28 19:09:10 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 //Author: Jacob Munly
 //The purpose of this file (for now) is to have an entry point for our program so that it can compile
-
+//It includes all of our class header files
+//To compile, run this command: g++ main.cpp -o Test_Executable
+//Then run Test_Executable
 #include <iostream>
 #include "FacultyResearcher.h"
 #include "LabManager.h"
