fe9b35f5c441d913f77b212e474f2435b9a53121
gilbertboys <91152662+gilbertboys@users.noreply.github.com>
2025-10-28 18:29:09 -0700

    Create FacultyResearcher.h

---
+++ src/FacultyResearcher.h
@@ -0,0 +1,25 @@
+// CS-2: FacultyResearcher.h
+// Auth: Dunlap, Jack
+// Supports: UR-101, UR-102, UR-103, UR-104
+// Collaborators: ResearchStudent[1..*], Assets[*], Reservation[*], Documents[*] 
+
+#include <string>
+#include <vector>
+
+class FacultyResearcher {
+private:
+  int FacultyID;
+  int labGroupID;
+public:
+  std::string Fname;
+  std::string Lname;
+  std::string email;
+  bool permissions;
+	
+	//make a reservation for an asset
+	bool reserveAsset(int assetID, bool permissions); //returns bool - was it successful in making reservation}
+	//produce a usage report for resource usage by a group
+	Document generateUsageReport(Asset assetUsed, Document dataRecords);//returns document structure
+	//display a record of the group the faculty is in charge of and their work they have done
+	void viewGroup(int LabGroupID);
+};
