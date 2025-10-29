1644e2c54fd4c2494cda592eb4bdb50809c52a26
gilbertboys <91152662+gilbertboys@users.noreply.github.com>
2025-10-28 18:29:47 -0700

    Create LabManager.h

---
+++ src/LabManager.h
@@ -0,0 +1,27 @@
+//Author:
+
+#include <string>
+#include <vector>
+
+class LabManager {
+private:
+    int labManagerID;
+    std::string privilage_changes_log[];
+public:
+	// prints LabAssetManager info
+    LabAssetManager return_lab_asset_manager_info(LabAssetManager obj);
+	// Grants lab asset manager rights
+    void grant_lab_asset_manager_rights();
+    // revokes lab asset manager rights
+    void revoke_lab_asset_manager_rights();
+    // prints all lab asset managers
+    std::string view_all_lab_asset_managers();
+    // allows access to reservations
+    void access_reservations();
+    // shows user activity
+    void access_user_activity();
+    // summarizes data and outputs it
+    std::string summarize_data();
+    // generates reports and outputs them
+    std::string generate_reports();
+};
