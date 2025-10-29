4ccd534b63389ff174a9bef65b386542d338f13d
gilbertboys <91152662+gilbertboys@users.noreply.github.com>
2025-10-28 18:48:49 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,9 @@
 //The purpose of this file (for now) is to have an entry point for our program so that it can compile
 
 #include <iostream>
-#include "Assets.h"
-#include "Documents.h"
 #include "FacultyResearcher.h"
-#include "LabAssetManager.h"
 #include "LabManager.h"
 #include "ResearchStudent.h"
-#include "Reservations.h"
 
 int main(){
   std::cout << "Compiled without errors" << std::endl;
