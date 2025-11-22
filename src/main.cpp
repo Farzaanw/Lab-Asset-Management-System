3d295c950791d296b5fad2d5d40c7b06c0e2d4a8
Kai Johnson <johnkai5@oregonstate.edu>
2025-11-22 01:25:45 -0800

    Redid main to serve as a testing template
    Everything in main will not get saved when you commit all your files. this is just to make it easier to test things!

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
-//Author: Jacob Munly
-//The purpose of this file (for now) is to have an entry point for our program so that it can compile
-//It includes all of our class header files
 //To compile, run this command: g++ main.cpp -o Test_Executable
-//Then run Test_Executable
+//Any changes made in this file will not be saved to git (visit .gitignore to change)
+//   This is to make it so testing is easier! please add #include files whenever you would like to test something
+//   Once we are ready to put everything together delete "src/*.exe" from .gitignore
 #include <iostream>
-#include "FacultyResearcher.h"
-#include "LabManager.h"
-#include "ResearchStudent.h"
 
 int main(){
-  std::cout << "Compiled without errors" << std::endl;
-  return 0;
+    // Add testing code here:
+
+    system("pause");
+    return 1; 
 }
