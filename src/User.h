c5034693162599bd31a3e9c0d21c968841b57d61
Kai Johnson <johnkai5@oregonstate.edu>
2025-11-22 01:18:04 -0800

    Added user header

--- src/User.h
+++ src/User.h
@@ -4,6 +4,7 @@ class User {
 private:
     std::string firstName;
     std::string lastName;
+    std::string role;
     std::string email;
     std::string password;
 
@@ -12,15 +13,4 @@ public:
     User(const std::string& f, const std::string& l,
          const std::string& e, const std::string& p)
         : firstName(f), lastName(l), email(e), password(p) {}
-
-    std::string getFirstName() const { return firstName; }
-    std::string getLastName() const { return lastName; }
-    std::string getEmail() const { return email; }
-
-    std::string getPassword() const { return password; }
-
-    void setFirstName(const std::string& f) { firstName = f; }
-    void setLastName(const std::string& l) { lastName = l; }
-    void setEmail(const std::string& e) { email = e; }
-    void setPassword(const std::string& p) { password = p; }
 };
