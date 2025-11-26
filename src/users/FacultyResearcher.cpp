d0bcd4f3e406807c945cf54d6117488247dde8ca
Jdunlap0004 <jack.dunlap04@gmail.com>
2025-11-25 23:13:02 -0800

    FM cpp/h Files
    Created the h file with all the required functions for the most part and just need to implement them into the cpp file next.

---
+++ src/users/FacultyResearcher.cpp
@@ -0,0 +1,52 @@
+#include "FacultyResearcher.h"
+
+FacultyResearcher() = default;
+
+//Destructor
+~FacultyResearcher() {
+  std::cout << "Destructor Called" << std::endl;
+}
+
+//BASIC
+//Navigation, displays the dashboard and the user actions
+void display_page();
+void user_actions();
+
+//ASSETS
+//make a reservation for an asset, returns bool - was it successful in making reservation}
+//same as use asset, when they reserve it, we are assuming they are using it
+bool reserveAsset(int assetID, bool permissions);
+//Return an asser
+bool return_asset(int reservationID);
+// view a student's list of assets they have checked out
+List<Assets> viewStudentAssets(int studentID);
+
+//GROUP SECTION
+//view the groups list of assets they have checked out
+List<Assets> viewGroupAssets(int labGroupID);
+//view group usage records(their usage log)
+void viewGroupUsage(int labGroupID);
+//display a record of the group the faculty is in charge of and their work they have done
+void viewGroup(int LabGroupID);
+
+//SOFTWARE LICENSE
+//View the available software licenses
+List<SoftwareLicense> viewAvailableLicenseSeats();
+//Request software licenses for self, returns a bool for if its successful or not
+bool requestSoftwareLicense(int licenseID, const std::string& startDate, const std::string& endDate);
+//Request software licesnes for group
+bool requestSoftwareLicenseGroup(int licenseID, int labGroupID, const std::string& startDate, const std::string& endDate);
+//view their own licenses
+List<SoftwareLicense> viewLicenses();
+//view a groups licenses
+List<SoftwareLicense> viewGroupLicenses(int labGroupID);
+
+//Reservations
+//View Group reservations
+List<Reservations*> viewGroupReservations(int labGroupID);
+//Manage Group reservations
+bool manageGroupReservations(int labGroupID, int reservationID /*Add Info needed to manage reservation*/);
+
+//DOCUMENTS
+//produce a usage report for resource usage by a group
+Documents generateUsageReport(Assets assetUsed, Documents dataRecords);
--- src/users/FacultyResearcher.h
+++ src/users/FacultyResearcher.h
@@ -1,3 +1,6 @@
+#ifndef FACULTYRESEARCHER_H
+#define FACULTYRESEARCHER_H
+
 // CS-1: FacultyResearcher.h
 // Auth: Dunlap, Jack
 // Supports: UR-101, UR-102, UR-103, UR-104
@@ -7,22 +10,69 @@
 #include <vector>
 #include "Assets.h"
 #include "Documents.h"
+#include "Reservations.h"
+#include "User.h"
 #pragma once
 
 class FacultyResearcher {
 private:
   int FacultyID;
   int labGroupID;
-public:
   std::string Fname;
   std::string Lname;
   std::string email;
   bool permissions;
 	
-	//make a reservation for an asset
-	bool reserveAsset(int assetID, bool permissions); //returns bool - was it successful in making reservation}
-	//produce a usage report for resource usage by a group
-	Documents generateUsageReport(Assets assetUsed, Documents dataRecords);//returns document structure
-	//display a record of the group the faculty is in charge of and their work they have done
+public:
+  //Constructor
+  FacultyResearcher() = default;
+
+  //Destructor
+  ~FacultyResearcher();
+
+  //BASIC
+  //Navigation, displays the dashboard and the user actions
+  void display_page();
+  void user_actions();
+
+  //ASSETS
+	//make a reservation for an asset, returns bool - was it successful in making reservation}
+  //same as use asset, when they reserve it, we are assuming they are using it
+	bool reserveAsset(int assetID, bool permissions);
+  //Return an asser
+  bool return_asset(int reservationID);
+  // view a student's list of assets they have checked out
+  List<Assets> viewStudentAssets(int studentID);
+
+  //GROUP SECTION
+  //view the groups list of assets they have checked out
+  List<Assets> viewGroupAssets(int labGroupID);
+  //view group usage records(their usage log)
+  void viewGroupUsage(int labGroupID);
+  //display a record of the group the faculty is in charge of and their work they have done
 	void viewGroup(int LabGroupID);
+
+  //SOFTWARE LICENSE
+  //View the available software licenses
+  List<SoftwareLicense> viewAvailableLicenseSeats();
+  //Request software licenses for self, returns a bool for if its successful or not
+  bool requestSoftwareLicense(int licenseID, const std::string& startDate, const std::string& endDate);
+  //Request software licesnes for group
+  bool requestSoftwareLicenseGroup(int licenseID, int labGroupID, const std::string& startDate, const std::string& endDate);
+  //view their own licenses
+  List<SoftwareLicense> viewLicenses();
+  //view a groups licenses
+  List<SoftwareLicense> viewGroupLicenses(int labGroupID);
+
+  //Reservations
+  //View Group reservations
+  List<Reservations*> viewGroupReservations(int labGroupID);
+  //Manage Group reservations
+  bool manageGroupReservations(int labGroupID, int reservationID /*Add Info needed to manage reservation*/);
+
+  //DOCUMENTS
+	//produce a usage report for resource usage by a group
+	Documents generateUsageReport(Assets assetUsed, Documents dataRecords);
 };
+
+#endif
