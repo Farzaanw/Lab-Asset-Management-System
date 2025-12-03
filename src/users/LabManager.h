7ddbcee387af1344f08d7bb84325032bd2cecb51
Kai Johnson <johnkai5@oregonstate.edu>
2025-12-02 17:35:49 -0800

    Created and fixed LabManager.cpp/.h
    Added Policies

--- src/users/LabManager.h
+++ src/users/LabManager.h
@@ -1,28 +1,33 @@
+//Author: Kai Johnson
+
 #include <string>
-#include <vector>
-#include "LabAssetManager.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <set>
+#include <filesystem>
 #include "User.h"
-#pragma once
+#include "../library/nlohmann/json.hpp"
+
+using namespace std;
+
+namespace fs = std::filesystem;
+using json = nlohmann::ordered_json;
 
 class LabManager : public User {
 private:
-    int labManagerID;
-    std::string privilage_changes_log[];
+	const string policiesFile = "../../data/policies.json";
+	
 public:
-	// prints LabAssetManager info
-    LabAssetManager return_lab_asset_manager_info(LabAssetManager obj);
-	// Grants lab asset manager rights
-    void grant_lab_asset_manager_rights();
-    // revokes lab asset manager rights
-    void revoke_lab_asset_manager_rights();
-    // prints all lab asset managers
-    std::string view_all_lab_asset_managers();
-    // allows access to reservations
-    void access_reservations();
-    // shows user activity
-    void access_user_activity();
-    // summarizes data and outputs it
-    std::string summarize_data();
-    // generates reports and outputs them
-    std::string generate_reports();
+	LabManager(const std::string& firstName = "",
+			   const std::string& lastName = "",
+			   const std::string& email = "",
+			   int role = 0)
+		: User(firstName, lastName, email, role) {}
+	//Max booking duration, advance booking horizon, and reservation priority rules.
+	bool listPolicies();
+
+	bool changePolicies();
+
+	int getPolicy(string policyType);
 };
